dc7a69f2bf434bd08d058c29aab5a2eecf1d0405
lk-online <54578040+lk-online@users.noreply.github.com>
2021-06-02 09:36:30 +0300

    .

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,12 +7,12 @@ router.get("/", function (req, res, next) {
   res.render (express) (Response.render) renders a template
   along with the values of named variables passed in an object
   res.render("index", { title: "Express" }); // index in public folder
-});
+}); this is a test
 */
 
 // GET home page.
-router.get('/', function(req, res) {
-  res.redirect('/actions');
+router.get("/", function (req, res) {
+  res.redirect("/actions");
 });
 
 module.exports = router;
